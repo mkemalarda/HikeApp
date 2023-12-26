51215c5c13aa2fca4c2171e7fc9bd76b653bce20
Mustafa Kemal ARDA <mkemalarda@gmail.com>
2023-12-26 17:51:57 +0300

    CustomButtonView added

--- HikeApp/Component/CardView.swift
+++ HikeApp/Component/CardView.swift
@@ -11,27 +11,65 @@ struct CardView: View {
     var body: some View {
         // MARK: - CARD
         
-        
         ZStack {
             CustomBackgroundView()
             
-            ZStack {
-                Circle()
-                    .fill(
-                        LinearGradient(colors: [Color("ColorIndigoMedium"),Color("ColorSalmonLight")], startPoint: .topLeading, endPoint: .bottomTrailing)
-                    )
+            VStack {
+                // MARK: - HEADER
                 
-                    .frame(width: 256, height: 256)
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                        .foregroundStyle(LinearGradient(colors: [.customGrayLight,.customGrayMedium], startPoint: .top, endPoint: .bottom))
+                        
+                        Spacer()
+                        
+                        Button {
+                            // ACTION: Show a Sheet
+                            print("The button was pressed.")
+                        } label: {
+                            CustomButtonView()
+                        }
+                        
+                    }
+                    
+                    Text("Fun and enjoyable outdoor activity for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                    
+                    
+                }
                 
-                Image("image-1")
-                    .resizable()
-                .scaledToFit()
+                //: HEADER
+                
+                .padding(.horizontal,30)
+                
+                
+                // MARK: - MAIN CONTENT
+                
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(colors: [Color("ColorIndigoMedium"),Color("ColorSalmonLight")], startPoint: .topLeading, endPoint: .bottomTrailing)
+                        )
+                    
+                        .frame(width: 256, height: 256)
+                    
+                    Image("image-1")
+                        .resizable()
+                    .scaledToFit()
+                }
             }
+            
+            // MARK: - FOOTER
+            
         }
         
         //: CARD
         .frame(width: 320, height: 570)
-        
         
     }
 }
