efb8fd90cd97c7e5a0860622c5ccce82c0ee0288
Mustafa Kemal ARDA <mkemalarda@gmail.com>
2023-12-26 14:27:19 +0300

    Initial Commit

---
+++ HikeApp/HikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAppApp.swift
+//  HikeApp
+//
+//  Created by Mustafa Kemal ARDA on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
